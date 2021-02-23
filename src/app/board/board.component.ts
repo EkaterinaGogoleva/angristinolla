1e7f90a6ab8aebfff51cfd3c144fd629a0b70f9d
Ekaterina Gogoleva <goglevaekaterina17@gmail.com>
2021-02-23 10:26:38 +0200

    до моих попыток сделать самостоятельно

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -14,6 +14,9 @@ export class BoardComponent implements OnInit {
     squares: any[]; // Taulukko jossa on eri tyyppisiä arvoja: null, 'X', '0'
     xIsNext: boolean; // Kertoo kumpi on seuraavaksi vuorossa
     winner: string; // Kertoo voittajan 'X' tai '0'
+    //boolean переменная, которая сообщает о ничьей
+    //переменная moves которая должна писать количество ходов
+
 
     constructor() { }
 
@@ -25,8 +28,12 @@ export class BoardComponent implements OnInit {
         // Kun uusi peli alkaa, pelin muuttujat alustetaan.
         // Squares-taulukkoon laitetaan 9 tyhjää paikkaa
         this.squares = Array(9).fill(null);
-        this.xIsNext = true;
+//перенести сюда на место true  предложение, которая edellisen pelin hävinnyt saa aloittaa
+//делаем, чтобы можно было выиграть не заполняя все поля
+        this.xIsNext = true; 
         this.winner = null;
+        //ничья on false
+        //moves вначале 0
     }
 
     /*
@@ -49,16 +56,23 @@ export class BoardComponent implements OnInit {
 
     // makeMove(index: number) laittaa ristin tai nollan squares -taulukkoon indeksiin index
     makeMove(index: number) {
+
+//ecли игра закончилась делаем return кщторый не может больше делать siirtoa, koska ollaan
+//poistuttu metodista
+
         // Paikan johon risti tai nolla laitetaan pitää olla tyhjä, eli null
         if (!this.squares[index]) {
             // splice-metodi poistaa indeksistä alkion ja laittaa
             // tilalle yhden alkion joka tulee this.player -get propertyltä
             this.squares.splice(index, 1, this.player);
             this.xIsNext = !this.xIsNext; // Vaihdetaan vuoroa
+            //this.moves++;
         }
         // Yritetään määritellä voittaja. Metodi tuottaa 'X', '0' tai null
         // tilanteesta riippuen. Jos voittaja on olemassa, se näytetään templaatissa.
         this.winner = this.calculateWinner();
+        //здесь выясняется есть ли победитель и сделаны ли все ходы onko kaikki siirrot tehty
+        //jos näin on , niin tasapeli on true
 
     }
     // Metodi joka määrittää pelin voittajan
